a6c5c1f497c7db733cee8a1bbf34cfd71be04b03
dmunofer <diego.fmz@hotmail.com>
2022-06-02 12:59:50 +0200

    pregunta d y main

---
+++ main.py
@@ -0,0 +1,15 @@
+from clase_Tweet import Tweet
+from clase_Timeline import Timeline
+from clase_DirectMessage import DirectMessage
+from clase_Retweet import Retweet
+
+#d)
+#Habrá que modificar tanto timeline y tweets de UserAccount para poder incluir la funcionalidad de retwittear, ya que esta se nutre tanto de los tweets que copia como de la timeline del usuarios y de sus followers en las que se publica
+
+
+#No hará falta modificarlo ya que los retweets son del mismo formato que lo tweets y tienen la misma restricción a 140 caractres por lo que actuará de la misma manera que un tweet normal, actualizandose en los tweets del usuario y la timeline de los followers
+
+
+    
+
+
